e1d46a400be807de3af7601a283816131ecd0dc4
Aktar Zaman <aktarzaman@berkeley.edu>
2019-11-15 23:10:29 -0800

    working on creating model

--- explain_algorithms/algo_explained/models.py
+++ explain_algorithms/algo_explained/models.py
@@ -3,3 +3,16 @@ from django.db import models
 # Create your models here.
 class Catagory(models.Model):
     name = models.CharField(max_length = 20)
+
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
+    catagories = models.ManyToManyField('Category', related_name = "posts")
+
+class Comment(models.Model):
+    author = models.CharField(max_length = 60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", one_delete = models.CASCADE)
