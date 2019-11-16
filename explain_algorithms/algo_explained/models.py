6f7edb9c55cb473e50322478f9fe8f9c35625352
Aktar Zaman <aktarzaman@berkeley.edu>
2019-11-15 23:24:22 -0800

    making migrations

--- explain_algorithms/algo_explained/models.py
+++ explain_algorithms/algo_explained/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Catagory(models.Model):
-    name = models.CharField(max_length = 20)
+class Category(models.Model):
+    name            = models.CharField(max_length = 20)
 
 class Post(models.Model):
-    title = models.CharField(max_length = 255)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add = True)
-    last_modified = models.DateTimeField(auto_now = True)
-    catagories = models.ManyToManyField('Category', related_name = "posts")
+    title           = models.CharField(max_length = 255)
+    body            = models.TextField()
+    created_on      = models.DateTimeField(auto_now_add = True)
+    last_modified   = models.DateTimeField(auto_now = True)
+    catagories      = models.ManyToManyField('Category', related_name = "posts")
 
 class Comment(models.Model):
-    author = models.CharField(max_length = 60)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey("Post", one_delete = models.CASCADE)
+    author          = models.CharField(max_length = 60)
+    body            = models.TextField()
+    created_on      = models.DateTimeField(auto_now_add=True)
+    post            = models.ForeignKey("Post", on_delete = models.CASCADE)
