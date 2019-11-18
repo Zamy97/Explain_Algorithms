2a880c9bd8fb5ef3d6ca18d33734ab941d3611a1
Aktar Zaman <aktarzaman@berkeley.edu>
2019-11-18 13:15:09 -0800

    working on trying to add a new app

--- explain_algorithms/algo_explained/apps.py
+++ explain_algorithms/algo_explained/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AlgoExplainedConfig(AppConfig):
     name = "explain_algorithms.algo_explained"
 
-    # verbose_name = "Blog_post"
+    verbose_name = "Blog_post"
