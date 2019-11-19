d9d233f6205b27e7b04747834e89f310d9934af9
Aktar Zaman <aktarzaman@berkeley.edu>
2019-11-18 18:07:16 -0800

    lost on django

--- explain_algorithms/algo_explained/views.py
+++ explain_algorithms/algo_explained/views.py
@@ -5,42 +5,46 @@ from explain_algorithms.algo_explained.forms import CommentForm
 #blog_index will display a list of all your posts.
 
 def blog_index(request):
-    posts = Post.objects.all().order_by("-created_on")
-    context = {
-        "posts" : posts,
-    }
+    context = "hello"
     return render(request, "blog_index.html", context)
 
+# def blog_index(request):
+#     posts = Post.objects.all().order_by("-created_on")
+#     context = {
+#         "posts" : posts,
+#     }
+#     return render(request, "blog_index.html", context)
 
-#blog_detail will display the full post as well as comments and a form to allow users to create new comments.
-def blog_category(request, category):
-    posts = Post.objects.filter(
-        categories__name__contains=category
-    ).order_by(
-        '-created_on'
-    )
-    context = {
-        "category": category,
-        "posts": posts
-    }
-    return render(request, "blog_category.html", context)
-
-
-#blog_category will be similar to blog_index, but the posts viewed will only be of a specific category chosen by the user.
-def blog_detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    comments = Comment.objects.filter(post=post)
-
-    form = CommentForm()
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = Comment(
-                author=form.cleaned_data["author"],
-                body=form.cleaned_data["body"],
-                post=post,
-            )
-            comment.save()
-
-    context = {"post": post, "comments": comments, "form": form}
-    return render(request, "blog_detail.html", context)
+#
+# #blog_detail will display the full post as well as comments and a form to allow users to create new comments.
+# def blog_category(request, category):
+#     posts = Post.objects.filter(
+#         categories__name__contains=category
+#     ).order_by(
+#         '-created_on'
+#     )
+#     context = {
+#         "category": category,
+#         "posts": posts
+#     }
+#     return render(request, "blog_category.html", context)
+#
+#
+# #blog_category will be similar to blog_index, but the posts viewed will only be of a specific category chosen by the user.
+# def blog_detail(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     comments = Comment.objects.filter(post=post)
+#
+#     form = CommentForm()
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = Comment(
+#                 author=form.cleaned_data["author"],
+#                 body=form.cleaned_data["body"],
+#                 post=post,
+#             )
+#             comment.save()
+#
+#     context = {"post": post, "comments": comments, "form": form}
+#     return render(request, "blog_detail.html", context)
