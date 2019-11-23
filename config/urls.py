c5d5a28fb6c32d088491ab04ba61239d26510ad4
Aktar Zaman <aktarzaman@berkeley.edu>
2019-11-23 15:13:56 -0800

    view is working

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("users/", include("explain_algorithms.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path("", include("explain_algorithms.algo_explained.urls", namespace = "Explain_Algo")),
+    path("", include("explain_algorithms.algo_explained.urls", namespace = "algorithms")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- explain_algorithms/algo_explained/urls.py
+++ explain_algorithms/algo_explained/urls.py
@@ -6,9 +6,9 @@ from django.urls import path
 # )
 from . import views
 
-app_name = "Explain_Algo"
+app_name = "algorithms"
 urlpatterns = [
         path("all_posts/", views.blog_index, name="blog_index"),
-        # path("<int:pk>/", views.blog_detail, name="blog_detail"),
-        # path("<category>/", views.blog_category, name="blog_category"),
+        path("<int:pk>/", views.blog_detail, name='blog_detail'),
+        path("<category>/", views.blog_category, name='blog_category'),
 ]
