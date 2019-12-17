364009c6e63433dd3ef7cef6e2ed21f5fa6987a2
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-16 22:03:51 -0800

    trying to figure out how to show the post in a card

--- explain_algorithms/projects/urls.py
+++ explain_algorithms/projects/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "projects"
 urlpatterns = [
-    path("", views.project_index, name = "project_index"),
+    path("projects/", views.project_index, name = "project_index"),
     path("<int:pk>/", views.project_detail, name = "project_detail"),
 ]
