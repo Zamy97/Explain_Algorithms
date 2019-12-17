159d6cc3961207fc9f3f8baf5ec7b08455996bef
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-16 21:18:17 -0800

    finally got it to show up on the home page

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
@@ -16,7 +16,7 @@ urlpatterns = [
     path("users/", include("explain_algorithms.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path("all_posts/", include("explain_algorithms.algo_explained.urls", namespace = "algorithms")),
+    path("", include("explain_algorithms.algo_explained.urls", namespace = "algorithms")),
     path("projects/", include("explain_algorithms.projects.urls", namespace = "projects")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
