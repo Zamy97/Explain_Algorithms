752601c60f49546afa0a0efeaadf2d5fb1b39c84
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-18 00:51:23 -0800

    working on adding a search bar

--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,7 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    # path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
