46fb87630222c8262a893250258e0ff8d901c907
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-25 12:32:41 -0800

    trying to figure out whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -17,7 +17,7 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 # This allows easy placement of apps within the interior
 # explain_algorithms directory.
@@ -35,7 +35,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
