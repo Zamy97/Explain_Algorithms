7f5f77df5862b0b520ff55ef1a0963426a9312bf
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-26 23:52:53 -0800

    fixing whitenoise

--- config/settings/production.py
+++ config/settings/production.py
@@ -148,7 +148,6 @@ ANYMAIL = {
 # http://whitenoise.evans.io/en/latest/django.html#enable-whitenoise
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa F405
 
-INSTALLED_APPS += ["whitenoise"]  # noqa F405
 
 
 # LOGGING
